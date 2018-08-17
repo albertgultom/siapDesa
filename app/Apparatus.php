25b278c2884e93cfce143f19030ce55e9ae046d3
albertgultom <gultomic@gmail.com>
2018-08-17 22:58:06 +0700

    model update

--- app/Apparatus.php
+++ app/Apparatus.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Apparatus extends Model
 {
-    //
+    protected $table = 'posts';
+    protected $guarded = [];
+    protected $fillable = [];
+    // protected $hidden = [];
 }
