cb9c4b7e62088efba14e3dc3f7cc910d2b8c2bd4
albertgultom <gultomic@gmail.com>
2018-09-23 18:15:35 +0700

    prerelease #3

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Desa Kujangsari</title>
+  <title>{{ config('app.name', 'Laravel') }}</title>
   <!-- Fontfaces CSS-->
   <link href="{{ asset('css/font-face.css') }}" rel="stylesheet" media="all">
   <link href="{{ asset('vendor/font-awesome-4.7/css/font-awesome.min.css') }}" rel="stylesheet" media="all">
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Desa Kujangsari</title>
+  <title>{{ config('app.name', 'Laravel') }}</title>
   <link rel="shorcut icon" href="{{ asset('storage/images/favicon.png') }}">
   <!-- Fonts -->
   <link rel="dns-prefetch" href="https://fonts.googleapis.com">
