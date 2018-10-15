09777d1ec18ec614ba243f839a07e44309b21de6
rahmadikaaa <rahmadika.putera@gmail.com>
2018-10-15 19:43:55 +0700

    galeri foto public

--- resources/views/berita/foto.blade.php
+++ resources/views/berita/foto.blade.php
@@ -4,122 +4,38 @@
 <div class="row mt-3">
   <div class="col-md-9" style="border-right:solid 1px #eee;">
     <h4 class="mb-3">List Galeri Foto</h4>
-    <div class="gallery-photos ml-md-3 mb-5">
-      <span class="d-block p-2 mb-2 bg-primary text-white">Kegiatan Kepala Desa - Aktifitas 1</span>
-      <div class="d-flex flex-row flex-wrap">
-        <a 
-          href="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (1).jpeg')!!}"
-          data-fancybox="gallery"
-          data-caption="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (1).jpeg')!!}" alt="">
-        </a>
-        <a 
-          href="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (2).jpeg')!!}"
-          data-fancybox="gallery"
-          data-caption="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (2).jpeg')!!}" alt="">
-        </a>
-        <a 
-          href="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (3).jpeg')!!}"
-          data-fancybox="gallery"
-          data-caption="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (3).jpeg')!!}" alt="">
-        </a>
-        <a 
-          href="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (4).jpeg')!!}"
-          data-fancybox="gallery"
-          data-caption="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (4).jpeg')!!}" alt="">
-        </a>
-        <a 
-          href="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (5).jpeg')!!}"
-          data-fancybox="gallery"
-          data-caption="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (5).jpeg')!!}" alt="">
-        </a>
-        <a 
-          href="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (6).jpeg')!!}"
-          data-fancybox="gallery"
-          data-caption="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (6).jpeg')!!}" alt="">
-        </a>
-        <a 
-          href="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (7).jpeg')!!}"
-          data-fancybox="gallery"
-          data-caption="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (7).jpeg')!!}" alt="">
-        </a>
-        <a 
-          href="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (8).jpeg')!!}"
-          data-fancybox="gallery"
-          data-caption="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (8).jpeg')!!}" alt="">
-        </a>
-        <a 
-          href="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (9).jpeg')!!}"
-          data-fancybox="gallery"
-          data-caption="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (9).jpeg')!!}" alt="">
-        </a>
-        <a 
-          href="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (10).jpeg')!!}"
-          data-fancybox="gallery"
-          data-caption="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (10).jpeg')!!}" alt="">
-        </a>
-        <a 
-          href="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (11).jpeg')!!}"
-          data-fancybox="gallery"
-          data-caption="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (11).jpeg')!!}" alt="">
-        </a>
-        <a 
-          href="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (12).jpeg')!!}"
-          data-fancybox="gallery"
-          data-caption="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (12).jpeg')!!}" alt="">
-        </a>
-        <a 
-          href="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (13).jpeg')!!}"
-          data-fancybox="gallery"
-          data-caption="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (13).jpeg')!!}" alt="">
-        </a>
-        <a 
-          href="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (14).jpeg')!!}"
-          data-fancybox="gallery"
-          data-caption="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (14).jpeg')!!}" alt="">
-        </a>
-      </div>
-    </div>
-    <div class="gallery-photos mb-5">
-        <span class="d-block p-2 mb-2 bg-primary text-white">Kegiatan Kepala Desa - Aktifitas 2</span>
-        <div class="d-flex flex-row flex-wrap">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (15).jpeg')!!}" alt="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (16).jpeg')!!}" alt="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (17).jpeg')!!}" alt="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (18).jpeg')!!}" alt="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (19).jpeg')!!}" alt="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (20).jpeg')!!}" alt="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (21).jpeg')!!}" alt="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (22).jpeg')!!}" alt="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (23).jpeg')!!}" alt="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (24).jpeg')!!}" alt="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (25).jpeg')!!}" alt="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (26).jpeg')!!}" alt="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (27).jpeg')!!}" alt="">
-          <img src="{!!asset('storage/photos/KegiatanKepalaDesa/FTKaDes (28).jpeg')!!}" alt="">
-        </div>
+    <div class="row">
+        @foreach ($foto as $item)            
+      <a href="{{route('foto.lihat',$item->id)}}" class="col-12 col-lg-4 mb-2 berita">
+            <div class="card features">
+                
+              <img class="card-img-top" src="{{asset('storage/photos/'.$item['contents '])}}" alt="Card image cap">
+              {{-- {{$item['contents ']}} --}}
+              <div class="card-body">
+                <div class="media">
+                  <div class="media-body">
+                      <h4 class="card-title">{!! $item->name !!}</h4>
+                      <small class="text-right">{!! $item->updated_at !!}</small>
+                      <div>
+                        @foreach ($item->tags as $tag)
+                          <small class="badge badge-pill badge-secondary">{!! $tag->name !!}</small> 
+                        @endforeach
+                      </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </a>
+        @endforeach
       </div>
   </div>
-  <div class="col-md-3">
+  {{-- <div class="col-md-3">
     <h4>Daftar</h4>
     <ul>
       <li>Kegiatan Kepala Desa - Aktifitas 1</li>
       <li>Kegiatan Kepala Desa - Aktifitas 2</li>
     </ul>
-  </div>
+  </div> --}}
 </div>
 @endsection
 
@@ -128,5 +44,56 @@
   // $('[data-fancybox="gallery"]').fancybox({
     // Options will go here
   // });
+  $(document).on('click', '.view-detail', function(e){
+    e.preventDefault();
+    id = $(this).data('view');
+    $.ajax({
+      url: "/gallery/"+id,
+      method: "GET",
+      dataType: "json"
+    }).done(function(res) {
+      $('#viewModalLabel').text(res.name);
+      $('#viewModalType').text(res.type);
+      $('#viewModalDate').text(res.date);
+      $('#viewModalTags').empty();
+      $('#viewModalContents').empty();
+
+      $.each(res.tags, function(i, v){
+        $('#viewModalTags').append('<span class="ml-2 badge badge-info">'+v+'</span>');
+      });
+      
+      $.each(res.contents, function(i, v){
+        if(res.content == 'photo'){
+          media = `
+            <img 
+              src="{{asset('storage/photos/`+v.image+`')}}" 
+              class="card-img-top img-fluid" 
+              alt="Card image cap">
+          `;
+        }else{
+          media = `
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe 
+              class="embed-responsive-item" 
+              src="`+v.video+`" 
+              allowfullscreen></iframe>
+          </div>
+          `;
+        }
+        $('#viewModalContents').append(`
+          <div class="card mb-2 bg-light viewModal`+res.content+`">
+            `+media+`
+            <div class="card-body">
+              <p class="card-text">`+v.name+`</p>
+            </div>
+          </div>
+        `);
+      });
+      
+      $("#viewModal").modal('show');
+    }).fail(function( jqXHR, textStatus ) {
+      alert( "Request failed: " + textStatus );
+    });
+  });
 </script>
 @endpush
