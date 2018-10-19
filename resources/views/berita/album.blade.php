548ccd789aa91277f8b528d0bb73173ad9eaf4db
albertgultom <gultomic@gmail.com>
2018-10-19 13:14:29 +0700

    staging 19okt

---
+++ resources/views/berita/album.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.pustaka')
+
+@section('pustaka')
+<div class="row">
+    <div class="col-lg-9 d-block p-2 mb-3" style="border-right:solid 1px #eee;">
+      <div class="d-block p-2 mb-3" style="border-bottom: darksalmon solid 1pt;">
+          <h6 class="text-info">{!!$data->name!!}</h6>
+      </div>
+      <div class="gallery-photos ml-md-3 mb-5">
+        <div class="d-flex flex-row flex-wrap">
+          @foreach ($data->contents as $item)
+            <a 
+              href="{!!asset('storage/photos/'.$item->image)!!}"
+              data-fancybox="gallery"
+              data-caption="">
+              <img src="{!!asset('storage/photos/'.$item->image)!!}" alt="">
+            </a>
+          @endforeach
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-3 d-block">
+      <div class="row">
+        <div class="col mb-3">
+          <span class="d-block p-2 bg-primary text-white text-uppercase rounded">Daftar Galeri</span>
+          <ul class="list-group list-group-flush">
+            @foreach ($list as $item)
+              <a href="{{route('galeri.lihat',['content' => $content, 'name' => $item->name])}}">
+                <li class="list-group-item">{!!$item->name!!}</li>
+              </a>
+            @endforeach
+          </ul>
+        </div>
+      </div>
+    </div>
+</div>
+@endsection
--- resources/views/berita/galeri.blade.php
+++ resources/views/berita/galeri.blade.php
@@ -5,13 +5,51 @@
   <div class="col-lg-3">
     <ul class="list-group list-group-flush">
       <li class="list-group-item list-group-item-primary">KATEGORI</li>
-      {{-- @foreach ($tags as $item)
-        <a href="{{route('artikel',['tag' => $item->name])}}">
-          <li class="list-group-item" >{!!$item->name!!}</li>
-        </a>
-      @endforeach --}}
+      @foreach ($tags as $item)
+          <a href="{{route('galeri',['content' => $content, 'tag' => $item->name])}}">
+            <li class="list-group-item" >{!!$item->name!!}</li>
+          </a>
+      @endforeach
     </ul>
   </div>
-  <div class="col-lg-9"></div>
+  <div class="col-lg-9">
+    <div class="row">
+      @foreach ($data as $item)
+        <a href="{{route('galeri.lihat',['content' => $content, 'name' => $item['name']])}}" class="col-12 col-lg-4 mb-2 berita">
+          <div class="card features">
+            @if($item->contents->count() > 0)
+              @foreach ($item->contents as $k => $v)
+                @if($k == 0)
+                  <img class="card-img-top bg-gradient-ver"
+                    src="{{asset('/storage/photos/'.$v->image)}}"
+                    style="object-fit: contain;"
+                    alt="Card image cap">
+                @endif
+              @endforeach
+            @else
+              <img class="card-img-top bg-gradient-ver"
+                src="{{asset('/storage/images/mark.png')}}"
+                style="object-fit: contain;"
+                alt="Card image cap">
+            @endif
+            <div class="card-body">
+              <div class="media">
+                <div class="media-body">
+                  <h4 class="card-title">{!!$item['name']!!}</h4>
+                  <small class="text-right">{!!$item['updated_at']!!}</small>
+                  <div>
+                    @foreach ($item['tags'] as $tag)
+                      <small class="badge badge-pill badge-secondary">{!! $tag->name !!}</small> 
+                    @endforeach
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+      @endforeach 
+    </div>
+    <div>{{ $data->appends(Request::except('page'))->links() }}</div>
+  </div>
 </div>
 @endsection
