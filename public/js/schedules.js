d31acff1b54eb3135636e2ec08ca3d50f42b1bd9
albertgultom <gultomic@gmail.com>
2018-11-03 16:46:21 +0700

    update 3.nov

---
+++ public/js/schedules.js
@@ -0,0 +1,11 @@
+var jadwal = [
+  {
+    "id"  : "Desa Kujangsari",
+    "date": "2018-11-04 23:00:00",
+    "msg" : "Update version to #1.0.0"
+  },{
+    "id"  : "Siapdesa",
+    "date": "2018-11-21",
+    "msg" : "Fix Environments"
+  }
+]
