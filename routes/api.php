88763487e32e49c4c79feb0d6f41893a603173de
albertgultom <gultomic@gmail.com>
2018-11-20 20:50:19 +0700

    update staging 20Nov

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::get('/tag/{id}', 'TagController@show');
+Route::post('/tes', function(Request $request){
+  return response()->json($request);
+});
