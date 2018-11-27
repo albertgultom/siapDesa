9c575c4494fc1ff596606091e40e6d4b037f975a
bryanstyawan <bryanstyawan@hotmail.com>
2018-11-27 21:09:22 +0700

    gallery public (video)

--- resources/views/berita/galeri.blade.php
+++ resources/views/berita/galeri.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.pustaka')
-
+@php
+//  dd($content);
+@endphp
 @section('pustaka')
 <div class="row">
   <div class="col-lg-3">
@@ -17,20 +19,40 @@
       @foreach ($data as $item)
         <a href="{{route('galeri.lihat',['content' => $content, 'name' => $item['name']])}}" class="col-12 col-lg-4 mb-2 berita">
           <div class="card features">
-            @if($item->contents->count() > 0)
-              @foreach ($item->contents as $k => $v)
-                @if($k == 0)
-                  <img class="card-img-top bg-gradient-ver"
-                    src="{{asset('/storage/photos/'.$v->image)}}"
-                    style="object-fit: contain;"
-                    alt="Card image cap">
-                @endif
-              @endforeach
-            @else
-              <img class="card-img-top bg-gradient-ver"
-                src="{{asset('/storage/images/mark.png')}}"
-                style="object-fit: contain;"
-                alt="Card image cap">
+            @if($content == 'photo')
+              @if($item->contents->count() > 0)
+                @foreach ($item->contents as $k => $v)
+                  @if($k == 0)
+                    <img class="card-img-top bg-gradient-ver"
+                      src="{{asset('/storage/photos/'.$v->image)}}"
+                      style="object-fit: contain;"
+                      alt="Card image cap">
+                  @endif
+                @endforeach
+              @else
+                <img class="card-img-top bg-gradient-ver"
+                  src="{{asset('/storage/images/mark.png')}}"
+                  style="object-fit: contain;"
+                  alt="Card image cap">
+              @endif
+            @elseif($content == 'video')
+              @if($item->contents->count() > 0)            
+                @foreach ($item->contents as $k => $v)
+                  @if($k == 0)
+                    <div class="embed-responsive embed-responsive-16by9">
+                      <iframe 
+                        class="embed-responsive-item" 
+                        src="{{$v->video}}" 
+                        allowfullscreen></iframe>
+                    </div>                  
+                  @endif
+                @endforeach
+              @else
+                <!-- <img class="card-img-top bg-gradient-ver"
+                  src="{{asset('/storage/images/mark.png')}}"
+                  style="object-fit: contain;"
+                  alt="Card image cap"> -->
+              @endif            
             @endif
             <div class="card-body">
               <div class="media">
